8ee2089a687adc3f4e71bc9fb4498b5dc092fc12
Atri Adhikari <atriadhikarinsec@gmail.com>
2021-02-25 11:05:37 +0530

    Updated serverFinal 25-02-21

--- serverFinal.cpp
+++ serverFinal.cpp
@@ -14,23 +14,19 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-        int server, serverSD, n;
+        int client, serverSD, read_client_message;
         struct sockaddr_in address;
         int opt = 1;
         int addlen = sizeof(address);
-        char msg[10] = {0};
-
         cout << "Connected" << endl;
-
         if ((serverSD = socket(AF_INET, SOCK_STREAM, 0)) == 0)
         {
-                cout << "NO CONNECTION...!" << endl;
+                cout << "<-- Connection UnStable -->" << endl;
                 exit(EXIT_FAILURE);
         }
-
         if (setsockopt(serverSD, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
         {
-                cout << "Setsockopt" << endl;
+                cout << "<-- Setsockopt -->" << endl;
                 exit(EXIT_FAILURE);
         }
         address.sin_family = AF_INET;
@@ -38,53 +34,51 @@ int main(int argc, char const *argv[])
         address.sin_port = htons( PORT );
         if (bind(serverSD, (struct sockaddr *)&address, sizeof(address)) < 0)
         {
-                cout << "binding Failed" << endl;
+                cout << "<-- Binding Error -->" << endl;
                 exit(EXIT_FAILURE);
         }
         if (listen(serverSD, 10) < 0)
         {
-                cout << "Listen" << endl;
+                cout << "<-- Listening Failed -->" << endl;
                 exit(EXIT_FAILURE);
         }
-        if ((server = accept(serverSD, (struct sockaddr *)&address, (socklen_t*)&addlen)) < 0)
+        if ((client = accept(serverSD, (struct sockaddr *)&address, (socklen_t*)&addlen)) < 0)
         {
-                cout << "Accept" << endl;
+                cout << "<-- Not Accepted -->" << endl;
                 exit(EXIT_FAILURE);
         }
-        char cm[10];
-        char sm[10] = "HI";
-        n = read( server, cm, 10);
-        cout << cm << endl;
+        cout << "<-- START -->" << endl;
+        char client_message[10];
+        char server_message[10] = "Hey There";
+        read_client_message = read( client, client_message, 10);
+        cout << client_message << endl;
         struct timespec start, end;
-        send(server, sm, strlen(sm), 0);
-        cout << "Message sent..." << endl;
-        cout << "***************" <<  endl;
-        cout << "Stablished Connection" << endl;
-        cout << "Send Message" << endl;
-        cin.getline( sm, 80);
-        send(server, sm, strlen(sm), 0);
+        send(client, server_message, strlen(server_message), 0);
+        cin.getline( server_message, 10);
+        send(client, server_message, strlen(server_message), 0);
         while(1)
         {
                 clock_gettime(CLOCK_MONOTONIC, &start);
-                n = read(server, cm, 10);
-                cout << ">>>"  << cm << endl;
-                if(strcmp(cm, "QUIT") == 0)
+                read_client_message = read(client, client_message, 10);
+                cout << ">>>"  << client_message << "(Time_Taken_for_reply --> " << end.tv_sec - start.tv_sec << "sec)"<< endl;
+                if(strcmp(client_message, "QUIT") == 0)
                 {
                         break;
                 }
                 clock_gettime(CLOCK_MONOTONIC, &end);
                 if (end.tv_sec - start.tv_sec > 20)
                 {
+                        coout << "Time taken to Lose connection --> " << end.tv_sec - start.tv_sec << "sec" << endl;
                         break;
                 }
-                cout << "reply --> " ;
-                cin.getline(sm, 10);
-                send(server, sm, strlen(cm), 0);
+                cout << "Reply --> " ;
+                cin.getline(server_message, 10);
+                send(client, server_message, strlen(server_message), 0);
                 cout << "Message sent..." << endl;
 
         }
         cout << "Connection closed...!" << endl;
         close(serverSD);
-        close(server);
+        close(client);
         return 0;
 }
